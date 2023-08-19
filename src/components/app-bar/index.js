949f131bf27273d2c74c7ad912dd5b9009418a5b
Gerardo <gerardo@risingcloud.com>
2023-08-19 15:08:28 -0700

    Added user Avatar and Logout

--- src/components/app-bar/index.js
+++ src/components/app-bar/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import { Avatar, IconButton, Menu, MenuItem, Stack, Toolbar, Tooltip, Typography } from '@mui/material'
 import MuiAppBar from "@mui/material/AppBar"
 import LiveTvIcon from '@mui/icons-material/LiveTv';
-import LogoutIcon from '@mui/icons-material/Logout';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const AppBar = () => {
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const { user, logout } = useAuth0();
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
         <MuiAppBar position="static">
             <Toolbar>
@@ -15,9 +26,31 @@ export const AppBar = () => {
                     alignItems="center">
                     <LiveTvIcon />
                     <Typography variant="h6">Streaming.tv</Typography>
-                    <IconButton sx={{ ml: "auto !important" }}>
-                        <LogoutIcon />
-                    </IconButton>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, ml: "auto !important" }}>
+                            <Avatar alt={user.name} src={user.picture} />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{ mt: '45px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        <MenuItem onClick={logout}>
+                            <Typography textAlign="center">Logout</Typography>
+                        </MenuItem>
+                    </Menu>
                 </Stack>
             </Toolbar>
         </MuiAppBar>
