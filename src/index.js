ccb320c4d295e4e36d2e7532db172b55a0c404f9
Gerardo <gerardo@risingcloud.com>
2023-08-19 13:24:02 -0700

    Implemented Auth0

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ import { HomePage } from './pages/homepage';
 import { LoginPage } from './pages/login';
 import { theme } from './utils/theme';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <LoginPage />
-    </ThemeProvider>
+    <Auth0Provider
+      domain="dev-xpih3wy6bfwa6fzy.us.auth0.com"
+      clientId="jzowyFweyiH3eGtxfJrlhtdLoK6kVpD7"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LoginPage />
+      </ThemeProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Button, Paper, Stack, Typography } from '@mui/material'
 import LiveTvIcon from '@mui/icons-material/LiveTv';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const LoginPage = () => {
+    const { loginWithRedirect, user } = useAuth0();
+
     return (
         <Stack
             component="main"
@@ -21,7 +24,11 @@ export const LoginPage = () => {
                 padding={"25px"}
                 spacing={1}>
                 <Typography variant="h5">Welcome</Typography>
-                <Button variant="outlined">Login</Button>
+                <Button
+                    variant="outlined"
+                    onClick={loginWithRedirect}>
+                    Login
+                </Button>
             </Paper>
         </Stack>
 
